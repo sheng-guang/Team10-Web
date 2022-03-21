f2c0ab4644b2745c88b249fcec8cd6cda6149c34
PsychoMaple <92438827+PsychoMaple@users.noreply.github.com>
2022-03-21 17:33:23 +0000

    basic

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Image Browsing' });
+  res.render('index', { title: 'Secret Task' });
+});
+
+router.get('/detail', function(req, res, next) {
+  res.render('detail', { title: 'Secret Task' });
 });
 
 module.exports = router;
