170f55ecf3bc9de1345bdcb9ed3e5fc7947f592b
Yixiang Wang <ywang597@sheffield.ac.uk>
2022-04-12 14:43:59 +0100

    Update story.js

--- public/javascripts/story.js
+++ public/javascripts/story.js
@@ -4,11 +4,15 @@ re={}
 re.title="story1";
 re.description="description1"
 re.picture="";
+re.author="author1"
+all.push(re);
+
+
 re2={}
 re2.title="story2";
 re.description="description2"
 re.picture="";
-all.push(re);
+re.author="author2";
 
 all.push(re2);
 
@@ -16,10 +20,11 @@ function getAllStory(){
     return all;
 }
 
-function addNewStory(title,picture,description){
+function addNewStory(title,picture,description,author){
     var ne={};
     ne.title=title;
     ne.picture=picture;
     ne.description=description;
+    ne.author=author;
     all.push(ne);
 }
