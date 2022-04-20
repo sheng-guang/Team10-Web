6ca6c47986cdd8863bda98eeaa3ec3c1727bb6de
acp21zo <92438827+PsychoMaple@users.noreply.github.com>
2022-04-20 17:20:07 +0100

    store data in the web

--- routes/story.js
+++ routes/story.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var chatModel = require('../models/chat');
 
+var story={};
+story.title='0'
+var list=[story];
+
+
 const chat = require('../controller/ChatController');
 const initDB= require('../controller/init');
 initDB.init();
@@ -10,14 +15,20 @@ router.get('/upload', function(req, res, next) {
     var title=req.query.title;
     var description=req.query.description;
     var author=req.query.author;
+    var newstory={};
+    newstory.title=title;
+    newstory.description=description;
+    newstory.author=author;
+    list.push(newstory);
+
+
     //mongo
     res.send();
 });
 
 router.get('/download', function(req, res, next){
     res.setHeader('Content-Type','application/json');
-    var re0={}
-    re0.title="O"
-    res.json([re0]);
+
+    res.json(list);
     }
 );
