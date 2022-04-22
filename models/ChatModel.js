41ac69060dc68d73a856eef4d24c4416c6981f08
acp21zo <92438827+PsychoMaple@users.noreply.github.com>
2022-04-22 19:25:24 +0100

    Update ChatModel.js

--- models/ChatModel.js
+++ models/ChatModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('../database/ChatDatabase');
+/**const mongoose = require('../database/ChatDatabase');
 
 const Schema = mongoose.Schema;
 
@@ -7,8 +7,8 @@ const ChatSchema = new Schema(
         ImageTitle:{type: String, max:20},//required true
         Description:{type: String, max:100},//required true
         Author: {type:String},
-        //BaseCode:{type:String}
+
     }
 );
 
-module.exports = mongoose.model('Chat',ChatSchema,'chat');
+module.exports = mongoose.model('Chat',ChatSchema,'chat');*/
