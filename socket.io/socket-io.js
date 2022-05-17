12b56b4b950abe3a0d6807e3d08b2eca52308cf5
acp21zo <92438827+PsychoMaple@users.noreply.github.com>
2022-05-17 15:16:06 +0100

    Update socket-io.js

--- socket.io/socket-io.js
+++ socket.io/socket-io.js
@@ -1,7 +1,5 @@
 exports.init = function(io) {
-  const chat= io
-      .of('/chat')
-      .on('connection', function (socket) {
+  io.sockets.on('connection', function (socket) {
     try {
 
 
@@ -12,11 +10,11 @@ exports.init = function(io) {
 
       socket.on('create or join', function (room, userId) {
         socket.join(room);
-        chat.to(room).emit('joined', room, userId);
+        io.sockets.to(room).emit('joined', room, userId);
       });
 
       socket.on('chat', function (room, userId, chatText) {
-        chat.to(room).emit('chat', room, userId, chatText);
+        io.sockets.to(room).emit('chat', room, userId, chatText);
       });
 
       socket.on('disconnect', function(){
